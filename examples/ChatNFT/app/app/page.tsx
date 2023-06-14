f439cdc6770127b9b738f7d61960a1d3785fa845
Takuya <taku@taku_win.localdomain>
2023-06-15 00:55:27 +0900

    updates

--- examples/ChatNFT/app/app/page.tsx
+++ examples/ChatNFT/app/app/page.tsx
@@ -1,11 +1,18 @@
 import { Inter } from 'next/font/google';
 import Form from '@/components/Form';
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <main className={inter.className}>
+      <Image
+        src="http://ipfs.io/ipfs/QmPbxeGcXhYQQNgsC6a36dDyYUcHgMLnGKnF8pVFmGsvqi"
+        alt="Picture from IPFS"
+        width={500}  // or the width you want
+        height={300} // or the height you want
+      />
       <Form />
     </main>
   );
